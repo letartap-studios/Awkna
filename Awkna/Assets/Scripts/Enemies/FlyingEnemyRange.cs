29bd0965ac1c2ff4f4b8ab0e776ef85d2bad7291
MihaiAnghelin <mihai.anghelin@gmail.com>
2019-04-27 11:54:41 +0300

    Flying enemy script edited

--- Awkna/Assets/Scripts/Enemies/FlyingEnemyRange.cs
+++ Awkna/Assets/Scripts/Enemies/FlyingEnemyRange.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 public class FlyingEnemyRange : MonoBehaviour
 {
-    private AIPath aIPath;
-    private AIDestinationSetter AIDestinationSetter;
-    private GameObject player;
+    private AIPath aIPath;                              // Refrence to the AIPath script on the enemy.
+    private AIDestinationSetter AIDestinationSetter;    // Refrence to the AIDestinationSetter script on the enemy.
+    private GameObject player;                          // Refence to the player object in the scene.
+    private bool playerInRange;
+    public LayerMask whatIsPlayer;
+    public float range;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +18,11 @@ public class FlyingEnemyRange : MonoBehaviour
         aIPath = GetComponent<AIPath>();
         AIDestinationSetter = GetComponent<AIDestinationSetter>();
         AIDestinationSetter.target = player.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        playerInRange = Physics2D.OverlapCircle(transform.position, )
     }
 
     private void OnTriggerEnter2D(Collider2D other)
