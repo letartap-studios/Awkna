708391ef6c2ce94147655ebe4647047274106b73
Tibi09 <49787431+Tibi09@users.noreply.github.com>
2019-05-01 19:49:37 +0300

    Added platforms
    The platforms are working.

--- Awkna/Assets/Scripts/Player Scripts/Platform.cs
+++ Awkna/Assets/Scripts/Player Scripts/Platform.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private PlatformEffector2D effector;
+    public float waitTime;                                      //variable that sets the time the player needs to press down before he can fall trough the platform
+
     void Start()
     {
-        
+        effector = GetComponent<PlatformEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.DownArrow))                  //if the Down button is pressed, the 'waitTime' period of time resets
+        {
+            waitTime = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (waitTime <= 0)                                  //if the player presses the down button for a 'waitTime' period of time, the platform collision will change
+            {
+                effector.rotationalOffset = 180f;               //the platform collision changes with 180 degrees
+                waitTime = 0f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;                     //decrease the period of time
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space))                        //if the player hits Space, the platform collision returns to its starting position (0 degrees)
+        {
+            effector.rotationalOffset = 0;                      //the platform collision returns
+        }
     }
 }
