f967428841dba8f3a50aeccd3397418376ade460
MihaiAnghelin <mihai.anghelin@gmail.com>
2019-05-01 15:25:40 +0300

    Box and Explosive Boxes
    - the player can now push the boxes.
    - almost explosive boxes

---
+++ Awkna/Assets/Scripts/Player Scripts/ExplosiveBox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ExplosiveBox : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isFalling;
+    private bool wasFalling;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        wasFalling = isFalling; 
+        if (rb.velocity.y > 0.5)
+        {
+            isFalling = true;
+        }
+        else
+        {
+            isFalling = false;
+        }
+
+        if (isFalling == false && wasFalling == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
