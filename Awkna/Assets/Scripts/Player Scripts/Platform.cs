4cf039e58ed46d49079f7aff422e76da0d94b06e
MihaiAnghelin <mihai.anghelin@gmail.com>
2019-05-01 22:36:27 +0300

    Changed the input for the platforms
    - changed the input for the platforms from GetKey to GetButton, so we can change the keys in the editor
    - tried to fix the collision between the boxes and the ground tiles

--- Awkna/Assets/Scripts/Player Scripts/Platform.cs
+++ Awkna/Assets/Scripts/Player Scripts/Platform.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
 
     private PlatformEffector2D effector;
-    public float waitTime;                                      //variable that sets the time the player needs to press down before he can fall trough the platform
+    public float waitTime;                                  //variable that sets the time the player needs to press down before he can fall trough the platform
 
     void Start()
     {
@@ -15,29 +13,29 @@ public class Platform : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.DownArrow))                  //if the Down button is pressed, the 'waitTime' period of time resets
-        {
+        if (Input.GetButtonUp("Vertical") && Input.GetAxisRaw("Vertical") < 0)
+        {                                                   //if the Down button is pressed, the 'waitTime' period of time resets
             waitTime = 0f;
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") < 0)
         {
-            if (waitTime <= 0)                                  //if the player presses the down button for a 'waitTime' period of time, the platform collision will change
+            if (waitTime <= 0)                              //if the player presses the down button for a 'waitTime' period of time, the platform collision will change
             {
-                effector.rotationalOffset = 180f;               //the platform collision changes with 180 degrees
+                effector.rotationalOffset = 180f;           //the platform collision changes with 180 degrees
                 waitTime = 0f;
             }
             else
             {
-                waitTime -= Time.deltaTime;                     //decrease the period of time
+                waitTime -= Time.deltaTime;                 //decrease the period of time
             }
         }
 
-        if (Input.GetKey(KeyCode.Space))                        //if the player hits Space, the platform collision returns to its starting position (0 degrees)
+        if (Input.GetButtonDown("Jump"))                        //if the player hits Space, the platform collision returns to its starting position (0 degrees)
         {
-            effector.rotationalOffset = 0;                      //the platform collision returns
+            effector.rotationalOffset = 0;                  //the platform collision returns
         }
-        if (Input.GetKey(KeyCode.K))
+        if (Input.GetButtonDown("SwitchGravity"))
         {
             effector.rotationalOffset = 0;
         }
