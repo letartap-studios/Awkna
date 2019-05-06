0936d8950f1ccc7c07865df1a846a787efcfeceb
Mihui <mihai.anghelin@gmail.com>
2019-05-06 17:39:33 +0300

    Worked on grappling hook
    - still in progress

---
+++ Awkna/Assets/Scripts/New Player/GrapplingHook.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float distance = 10f;
+    public LayerMask layerMask;
+
+    void Start()
+    {
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Grapple"))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, layerMask);
+
+            if(hit.collider!=null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+            }
+        }
+
+        if (Input.GetButtonUp("Grapple"))
+        {
+            joint.enabled = false;
+        }
+    }
+}
