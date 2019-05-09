71a31f217c73724ae55bc662c7e9cc36a1387fa3
Mihui <mihai.anghelin@gmail.com>
2019-05-09 23:14:17 +0300

    in Progress
    - workin on camera lerp
    - working on grappling hook

---
+++ Awkna/Assets/Scripts/New Player/RopeSystem.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class RopeSystem : MonoBehaviour
+{
+
+    // 1
+    public GameObject ropeHingeAnchor;
+    public DistanceJoint2D ropeJoint;
+    public Transform crosshair;
+    public SpriteRenderer crosshairSprite;
+    //public PlayerMovement playerMovement;
+    private bool ropeAttached;
+    private Vector2 playerPosition;
+    private Rigidbody2D ropeHingeAnchorRb;
+    private SpriteRenderer ropeHingeAnchorSprite;
+
+    void Awake()
+    {
+        // 2
+        ropeJoint.enabled = false;
+        playerPosition = transform.position;
+        ropeHingeAnchorRb = ropeHingeAnchor.GetComponent<Rigidbody2D>();
+        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 3
+        var worldMousePosition =
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        var facingDirection = worldMousePosition - transform.position;
+        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        if (aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+
+        // 4
+        var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        // 5
+        playerPosition = transform.position;
+
+        // 6
+        if (!ropeAttached)
+        {
+            SetCrosshairPosition(aimAngle);
+        }
+        else
+        {
+            crosshairSprite.enabled = false;
+        }
+
+    }
+    private void SetCrosshairPosition(float aimAngle)
+    {
+        if (!crosshairSprite.enabled)
+        {
+            crosshairSprite.enabled = true;
+        }
+
+        var x = transform.position.x + 1f * Mathf.Cos(aimAngle);
+        var y = transform.position.y + 1f * Mathf.Sin(aimAngle);
+
+        var crossHairPosition = new Vector3(x, y, 0);
+        crosshair.transform.position = crossHairPosition;
+    }
+
+
+}
