87f71c7273cf7cdbe5e9f19c25e79c1d48523e91
Mihui <mihai.anghelin@gmail.com>
2019-05-11 00:03:01 +0300

    Grappling Hook still in progess
    - making improvements with the grappling hook
    - having problems with the camera shaker

--- Awkna/Assets/Scripts/New Player/CameraFollow.cs
+++ Awkna/Assets/Scripts/New Player/CameraFollow.cs
@@ -3,9 +3,14 @@
 public class CameraFollow : MonoBehaviour
 {
     #region Variables
-    public Transform target;                              // The refrence of what camera follows.
-    public float smoothSpeed = 10f;                       // How much to smooth out the camera movement.
-    public Vector3 offset;                                // Offset of the camera.
+    //public Transform target;                              // The refrence of what camera follows.
+    //public float smoothSpeed = 10f;                       // How much to smooth out the camera movement.
+    //public Vector3 offset;                                // Offset of the camera.
+
+    public Transform target;
+    public float speed;
+    public float cameraDepth = -10f;
+    //public float minX, minY, maxX, maxY;
 
     public bool bounds;                                   // Whether the camera should have bounds or not, so it doesn't see out of the map.
     public Vector3 minCameraPos;                          // The minumum position at which the camera can go.
@@ -16,9 +21,17 @@ public class CameraFollow : MonoBehaviour
     {
         if (target.GetComponent<PlayerController>() != null)
         {
-            Vector3 desiredPosition = transform.position = target.position + offset;                                    // Follow the target
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); // Smooth the camera movement
-            transform.position = smoothedPosition;                                                                      // Set the position to the smoothed position
+            //Vector3 desiredPosition = transform.position = target.position + offset;                                    // Follow the target
+            //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime); // Smooth the camera movement
+            //transform.position = smoothedPosition;                                                                      // Set the position to the smoothed position
+
+            Vector2 newPosition = Vector2.Lerp(transform.position, target.position, Time.deltaTime * speed);
+            Vector3 camPosition = new Vector3(newPosition.x, newPosition.y, cameraDepth);
+            Vector3 v3 = camPosition;
+            //float newX = Mathf.Clamp(v3.x, minX, maxX);
+            //float newY = Mathf.Clamp(v3.y, minY, maxY);
+            transform.position = new Vector3(v3.x, v3.y, cameraDepth);
+
 
             if (bounds)                          // If the camera has bounds...
             {                                   //...set its maximum and minimum position.
--- Awkna/Assets/Scripts/New Player/RopeSystem.cs
+++ Awkna/Assets/Scripts/New Player/RopeSystem.cs
@@ -7,36 +7,34 @@ public class RopeSystem : MonoBehaviour
     public GameObject ropeHingeAnchor;
     public DistanceJoint2D ropeJoint;
     public Transform crosshair;
-    public SpriteRenderer crosshairSprite;
-    //public PlayerMovement playerMovement;
+    public SpriteRenderer crosshairSprite;    
     private bool ropeAttached;
     private Vector2 playerPosition;
     private Rigidbody2D ropeHingeAnchorRb;
     private SpriteRenderer ropeHingeAnchorSprite;
 
-    void Awake()
+    private void Awake()
     {
         // 2
         ropeJoint.enabled = false;
         playerPosition = transform.position;
         ropeHingeAnchorRb = ropeHingeAnchor.GetComponent<Rigidbody2D>();
-        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();
+        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();        
     }
-
-    void Update()
+    private void Update()
     {
         // 3
-        var worldMousePosition =
-            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
-        var facingDirection = worldMousePosition - transform.position;
-        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        Vector3 worldMousePosition =
+            Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 facingDirection = new Vector3(worldMousePosition.x, worldMousePosition.y, transform.position.z) - transform.position;
+        float aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
         if (aimAngle < 0f)
         {
-            aimAngle = Mathf.PI * 2 + aimAngle;
+            aimAngle += (2 * Mathf.PI);
         }
 
         // 4
-        var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        Vector3 aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
         // 5
         playerPosition = transform.position;
 
@@ -51,6 +49,7 @@ public class RopeSystem : MonoBehaviour
         }
 
     }
+
     private void SetCrosshairPosition(float aimAngle)
     {
         if (!crosshairSprite.enabled)
@@ -58,10 +57,10 @@ public class RopeSystem : MonoBehaviour
             crosshairSprite.enabled = true;
         }
 
-        var x = transform.position.x + 1f * Mathf.Cos(aimAngle);
-        var y = transform.position.y + 1f * Mathf.Sin(aimAngle);
+        float x = transform.position.x + 1f * Mathf.Cos(aimAngle);
+        float y = transform.position.y + 1f * Mathf.Sin(aimAngle);
 
-        var crossHairPosition = new Vector3(x, y, 0);
+        Vector3 crossHairPosition = new Vector3(x, y, 0);
         crosshair.transform.position = crossHairPosition;
     }
 
