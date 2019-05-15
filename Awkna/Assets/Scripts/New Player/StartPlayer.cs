1a861205eae79dc42f3a699a913c3213c9109d05
Mihui <mihai.anghelin@gmail.com>
2019-05-15 15:56:29 +0300

    Managed conflicts + player spawn
    - the player spawns at the location of the entrance

---
+++ Awkna/Assets/Scripts/New Player/StartPlayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class StartPlayer : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.FindWithTag("Player").transform.position = transform.position;
+    }
+}
