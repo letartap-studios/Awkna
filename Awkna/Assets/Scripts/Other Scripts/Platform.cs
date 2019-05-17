ecdb6a29a769eed1df9e840806db3a5b082b7af2
Mihui <mihai.anghelin@gmail.com>
2019-05-17 19:03:44 +0300

    Repaired the platforms
    - repaired the platforms
    - changed the collider of the player

--- Awkna/Assets/Scripts/Other Scripts/Platform.cs
+++ Awkna/Assets/Scripts/Other Scripts/Platform.cs
@@ -7,27 +7,34 @@ public class Platform : MonoBehaviour
     public float radius;
     public Vector2 offset;
     public LayerMask player;
-    private Vector2 pos;
-    private Collider2D other;
+    private Collider2D collider2D;
 
     private void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
-        pos = transform.position;
-        other = GetComponent<Collider2D>();
+        collider2D = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
-        //other = Physics2D.OverlapCircle(pos + offset, radius);
-        if (other.CompareTag("Player"))
+        CheckCollision();
+    }
+
+    private void CheckCollision()
+    {
+        bool other = collider2D.IsTouchingLayers(player);
+        if (!other)
+        {
+            return;
+        }
+        else
         {
             if (Input.GetAxis("Vertical") < 0)
             {
                 effector.rotationalOffset = 180f;
-            }                                                                   //if the player hits the Down button, the collision platform rotates 180 degrees,
-            else                                                                //letting the player to fall. Otherwise, the platform collision will only permit 
-            {                                                                   //jumping on it
+            }                                                 //if the player hits the Down button, the collision platform rotates 180 degrees,
+            else                                              //letting the player to fall. Otherwise, the platform collision will only permit 
+            {                                                 //jumping on it
                 effector.rotationalOffset = 0f;
             }
 
@@ -42,58 +49,4 @@ public class Platform : MonoBehaviour
             }
         }
     }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-
-        Gizmos.DrawWireSphere(pos + offset, radius);
-    }
 }
-
-/*public class Platform : MonoBehaviour
-{
-
-    private PlatformEffector2D effector;
-    public float waitTime;                                  //variable that sets the time the player needs to press down before he can fall trough the platform
-
-    void Start()
-    {
-        effector = GetComponent<PlatformEffector2D>();
-    }
-
-    void Update()
-    {
-        if (Input.GetButtonUp("Vertical") && Input.GetAxisRaw("Vertical") < 0)
-        {                                                   //if the Down button is pressed, the 'waitTime' period of time resets
-            waitTime = 0f;
-        }
-
-        if (Input.GetButton("Vertical") && Input.GetAxisRaw("Vertical") < 0)
-        {
-            if (waitTime <= 0)                              //if the player presses the down button for a 'waitTime' period of time, the platform collision will change
-            {
-                //changingTime = 0;
-                effector.rotationalOffset = 180f;           //the platform collision changes with 180 degrees
-                waitTime = 0f;
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;                 //decrease the period of time
-            }
-        }
-
-        if (Input.GetButtonDown("Jump"))                        //if the player hits Space, the platform collision returns to its starting position (0 degrees)
-        {
-            effector.rotationalOffset = 0;                  //the platform collision returns
-        }
-
-        if (Input.GetButtonDown("SwitchGravity"))           //
-        {
-            effector.rotationalOffset = 0;
-        }
-        
-
-    }
-}*/
-
