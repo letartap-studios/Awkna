433242e648a4ae778811800d8a6b02711a8f9956
David Galati <david.galati01@yahoo.com>
2019-06-03 16:44:01 +0300

    Shop Tiles + Bomb Packs + Shop Room Prefabs💰💰💰
     Felt cute, made a sepparate shop scene, might delete it later. After that, made in-map shop tiles. Based on the crate prefab, i made the shop tiles and gave the option to drop the new object called BombPack. Price, item type and qunatity selectable from in the editor

---
+++ Awkna/Assets/Scripts/Other Scripts/ShopItem.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    public GameObject[] objects;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private Vector2 size;
+    private LayerMask playerMask;
+    public Animator anim;
+    public int price;
+    public int numberOfItems;
+
+    private void Start()
+    {
+        playerMask = LayerMask.GetMask("Player");
+    }
+
+    public void DestroyShopItem()
+    {
+        for (int i = 1; i <= numberOfItems; i++) 
+        {
+            int rand = Random.Range(0, objects.Length);
+            Instantiate(objects[rand], transform.position, Quaternion.identity);
+            //AstarPath.active.Scan();
+        }
+        GameObject.FindWithTag("Player").GetComponent<RopeSystem>().ResetRope();
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        bool other = Physics2D.OverlapBox((Vector2)transform.position + offset, size, 0, playerMask);
+
+        if (!other)
+        {
+            return;
+        }
+        else
+        {
+            if (anim != null)
+            {
+                anim.SetBool("inRange", true);
+            }
+
+            if (Input.GetButtonDown("Interact"))
+            {
+                if (PlayerStats.Instance.GemNumber >= price)
+                {
+                    PlayerStats.Instance.PayGems(price);
+                    DestroyShopItem();
+                    
+                }
+            }
+        }
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (anim != null)
+                anim.SetBool("inRange", false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube((Vector2)transform.position + offset, size);
+    }
+}
