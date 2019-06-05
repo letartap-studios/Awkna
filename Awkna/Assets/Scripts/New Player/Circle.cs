35be71097d77ae22c47dffee893031eea2038be8
Mihui <mihai.anghelin@gmail.com>
2019-06-05 19:12:36 +0300

    Started the circle for grappling hook feedback

--- Awkna/Assets/Scripts/New Player/Circle.cs
+++ Awkna/Assets/Scripts/New Player/Circle.cs
@@ -2,5 +2,18 @@
 [RequireComponent(typeof(LineRenderer))]
 public class Circle : MonoBehaviour
 {
+    public int vertexCount = 40;
+    public float lineWidth = 0.2f;
+    public float radius;
+    public bool circleFillscreen;
 
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+#if UNITY_EDITOR
+    void 
 }
