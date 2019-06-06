2c231f293f6efb13b668ee244703afd1f1937801
Tibi09 <49787431+Tibi09@users.noreply.github.com>
2019-06-06 13:49:23 +0300

    Updated the tutorial
    Added new rooms and made a tutorial manager

---
+++ Awkna/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+    private float waitTime = 5f;
+    private float waitTimeJump = 0.08f;
+    private float waitTimePutBomb = 5f;
+    private float waitTimeGem = 5f;
+
+    void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+
+        if (popUpIndex == 0)
+        {
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                if (waitTimeJump <= 0)
+                {
+                    popUpIndex++;
+                }
+                else
+                {
+                    waitTimeJump -= Time.deltaTime;
+                    Debug.Log(waitTimeJump);
+                }
+            }
+        }else if (popUpIndex == 1)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                    popUpIndex++;
+            }
+        }else if (popUpIndex == 2)
+        {
+            if (waitTime <= 0)
+            {
+                popUpIndex++;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }else if (popUpIndex == 3)
+        {
+            if (waitTimePutBomb <= 0)
+            {
+                popUpIndex++;
+            }
+            else
+            {
+                waitTimePutBomb -= Time.deltaTime;
+            }
+        }else if (popUpIndex == 4)
+        {
+            if (Input.GetButtonDown("Bomb"))
+            {
+                if (waitTimeGem <= 0)
+                {
+                    popUpIndex++;
+                }
+                else
+                {
+                    waitTimeGem -= Time.deltaTime;
+                }
+            }
+        }
+    }
+}
