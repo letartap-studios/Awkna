7ccd0aca9e52f7e2389da51b68e6fe1a1431ab6a
David Galati <david.galati01@yahoo.com>
2019-06-07 00:26:43 +0300

    Finally -  shop tiles are fully working 😪😪😪😪
    During a stormy night full of swearing and Mihai turning into Godzilla because of the HORRIBLE GIT CONFLICTS, 500 whatsapp stickers later, we managed to somehow fix the GIT DEZASTER. The Git Kraken was realeased and the shop are rulled by the greedy alien Jews. xoxies good night 🤑🤑🤑

--- Awkna/Assets/Scripts/Other Scripts/ShopItem.cs
+++ Awkna/Assets/Scripts/Other Scripts/ShopItem.cs
@@ -2,28 +2,40 @@
 
 public class ShopItem : MonoBehaviour
 {
-    public GameObject[] objects;
+    public GameObject soldObject;
+
+    public Sprite soldObjectSprite;
+
+    public GameObject[] PickupArray;
+
+    public TextMesh PriceText;
+
     [SerializeField] private Vector2 offset;
     [SerializeField] private Vector2 size;
     private LayerMask playerMask;
     public Animator anim;
+    public Animator anim2;
     public int price;
     public int numberOfItems;
 
+    public GameObject displayItemPos;
+
+    
+
     private void Start()
     {
+        soldObjectSprite =  GetComponent<SpriteRenderer>().sprite;
+
         playerMask = LayerMask.GetMask("Player");
+        soldObject = PickupArray[Random.Range(1, PickupArray.Length)];
+        displayItemPos.GetComponent<SpriteRenderer>().sprite = soldObject.GetComponent<SpriteRenderer>().sprite;
+        PriceText.text = price.ToString();
     }
 
     public void DestroyShopItem()
     {
-        for (int i = 1; i <= numberOfItems; i++) 
-        {
-            int rand = Random.Range(0, objects.Length);
-            Instantiate(objects[rand], transform.position, Quaternion.identity);
-            //AstarPath.active.Scan();
-        }
-        GameObject.FindWithTag("Player").GetComponent<RopeSystem>().ResetRope();
+        Instantiate(soldObject, transform.position, Quaternion.identity);
+        //GameObject.FindWithTag("Player").GetComponent<RopeSystem>().ResetRope();
         Destroy(gameObject);
     }
 
@@ -40,6 +52,10 @@ public class ShopItem : MonoBehaviour
             if (anim != null)
             {
                 anim.SetBool("inRange", true);
+            }
+            if (anim2 != null)
+            {
+                anim2.SetBool("inRange", true);
             }
 
             if (Input.GetButtonDown("Interact"))
@@ -62,8 +78,11 @@ public class ShopItem : MonoBehaviour
         {
             if (anim != null)
                 anim.SetBool("inRange", false);
+            if (anim2 != null)
+                anim2.SetBool("inRange", false);
         }
     }
+
 #if UNITY_EDITOR
     private void OnDrawGizmosSelected()
     {
