ee9964ee0cef4ecdc2ab0919dd9c83031c454373
Mihui <mihai.anghelin@gmail.com>
2019-06-09 14:38:04 +0300

    Stomping Heads
    - the player can now jump on the enemies to kill them

--- Awkna/Assets/Scripts/Enemies/Flying Enemy/EnemyAttack.cs
+++ Awkna/Assets/Scripts/Enemies/Flying Enemy/EnemyAttack.cs
@@ -2,35 +2,14 @@
 
 public class EnemyAttack : MonoBehaviour
 {
-    private PlayerController player;
-    public float knockDuration = 0.5f;
-    public float knockbackPwr = 5;
     public float dealtDamage = 1;
-    public Vector2 offset;
-    public float radius;
-    private LayerMask playerMask;
-    [HideInInspector]
-    public bool attackTrigger;
-
-    private void Start()
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        playerMask = LayerMask.GetMask("Player");
-    }
-
-    private void Update()
-    {
-        attackTrigger = Physics2D.OverlapCircle((Vector2)transform.position + offset, radius, playerMask);
-
-        if (attackTrigger)
+        if (collision.CompareTag("Player"))
         {
             PlayerStats.Instance.TakeDamage(dealtDamage, transform.position);
         }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere((Vector2)transform.position + offset, radius);
     }
 
 }
